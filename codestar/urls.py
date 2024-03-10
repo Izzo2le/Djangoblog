a541e30e53a0bfdaf29abaab601acd96f4ab92cf
Izzo2le <isobel.otoole@aim.com>
2024-03-10 16:36:12 +0000

    update

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path, include
+from django.urls import path
+from blog import views
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
+    ...
+    path('blog/', views.hello_blog, name='hello_blog'),
 ]
